646822281b9fe4eaffdfbc8cb8a6009ea7cca6d8
Myat Phone Paye <myatphonepyae2016@gmail.com>
2026-09-17 16:44:27 +0700

    Complete Next JS 3 authentication

---
+++ backend/src/app/api/auth/login/route.js
@@ -0,0 +1,124 @@
+import corsHeaders from "@/lib/cors";
+import { getClientPromise } from "@/lib/mongodb";
+import { errorResponse } from "@/lib/utils";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const adminUser = process.env.ADMIN_USER;
+const adminPass = process.env.ADMIN_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+// Handle CORS preflight
+export function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
+}
+
+export async function POST(req) {
+  try {
+    const data = await req.json();
+    const { email, password } = data;
+
+    if (!email || !password) {
+      return errorResponse("Missing email or password", 400);
+    }
+
+    const admin = checkAdmin(email, password);
+    const user = !admin
+      ? await checkUser(email, password)
+      : admin;
+
+    if (!user) {
+      return errorResponse("Invalid email or password", 401);
+    }
+
+    // Generate JWT
+    const token = getJwtToken(user);
+
+    // Create response
+    const response = NextResponse.json(
+      {
+        message: "Login successful",
+        user: user,
+      },
+      {
+        status: 200,
+        headers: corsHeaders,
+      }
+    );
+
+    // Set JWT as HTTP-only cookie
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+      secure: false,
+    });
+
+    return response;
+  } catch (error) {
+    console.log("Login exception:", error);
+
+    return errorResponse("Login failed", 500);
+  }
+}
+
+function checkAdmin(email, password) {
+  if (!adminUser || !adminPass) {
+    return false;
+  }
+
+  if (adminUser === email && adminPass === password) {
+    return {
+      _id: "-1",
+      email: email,
+      username: "admin",
+    };
+  }
+
+  return false;
+}
+
+async function checkUser(email, password) {
+  try {
+    const client = await getClientPromise();
+    const db = client.db(DB_NAME);
+
+    const user = await db
+      .collection("user")
+      .findOne({ email });
+
+    if (!user) {
+      return false;
+    }
+
+    const check = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    return check ? user : false;
+  } catch (error) {
+    console.log("MongoDB exception:", error);
+    return false;
+  }
+}
+
+function getJwtToken(user) {
+  return jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+      username: user.username,
+    },
+    JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+}
